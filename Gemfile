e9dbbb5947042898e417b05b4fd711d1424156cc
ricardo <ricardopsilvafilho@gmail.com>
2025-06-05 15:28:37 -0300

    Prepara aplicação para deploy no Render com PostgreSQL

--- Gemfile
+++ Gemfile
@@ -6,6 +6,10 @@ gem "rails", "~> 8.0.2"
 gem "propshaft"
 # Use mysql as the database for Active Record
 gem "mysql2", "~> 0.5"
+
+group :production do
+  gem "pg", ">= 0.18", "< 2.0" # Para PostgreSQL no Render
+end
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,14 @@ Rails.application.configure do
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  # Para logging em plataformas como o Render
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
